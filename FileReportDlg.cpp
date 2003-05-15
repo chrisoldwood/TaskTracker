4f10f7e25a05ffd132c9b0a640e29cfd4dcd97bf
Administrator <administrator@chrisoldwood.com>
2003-05-15 09:50:09 +0000

    Changed ASSERT(false) to ASSERT_FALSE().

--- FileReportDlg.cpp
+++ FileReportDlg.cpp
@@ -67,7 +67,7 @@ void CFileReportDlg::OnInitDialog()
 		case ByWeek:	m_rbGroupByWeek.Check(true);	break;
 		case ByMonth:	m_rbGroupByMonth.Check(true);	break;
 		case ByTask:	m_rbGroupByTask.Check(true);	break;
-		default:		ASSERT(false);					break;
+		default:		ASSERT_FALSE();					break;
 	}
 	
 	// Fill period list.
