98e987884dbeb02547d81f4362d132dec396b6a8
Administrator <administrator@chrisoldwood.com>
2005-08-05 18:56:57 +0000

    Replaced use of a CPtrList of CSession* with a std::list<CSession*> for the collection and fixed up iteration code. Added CIsSessionInRange predicate class to replace the CSessionEnum class.

--- SessionList.hpp
+++ SessionList.hpp
@@ -16,10 +16,12 @@
 ** 
 ** This class is used to hold the list of sessions.
 **
+** NB: This class used to derive from CPtrList.
+**
 *******************************************************************************
 */
 
-class CSessionList : public CPtrList
+class CSessionList : public std::list<CSession*>
 {
 public:
 	//
@@ -31,9 +33,11 @@ public:
 	//
 	// Methods.
 	//
-	int  Add(CSession* pSession);
-	int  Remove(CSession* pSession);
+	void Add(CSession* pSession);
+	void Remove(CSession* pSession);
 	void RemoveAll();
+
+	int IndexOf(const CSession* pSession) const;
 
 	//
 	// File I/O.
@@ -48,10 +52,45 @@ protected:
 };
 
 /******************************************************************************
+** 
+** Predicate class used to determine if a session is in the given date range.
+**
+*******************************************************************************
+*/
+
+class CIsSessionInRange : public std::unary_function<const CSession*, bool>
+{
+public:
+	CIsSessionInRange(const CDateTime& dtStart, const CDateTime& dtEnd);
+
+	bool operator()(const CSession* pSession);
+
+private:
+	//
+	// Members.
+	//
+	CDateTime	m_dtStart;		// Start date.
+	CDateTime	m_dtEnd;		// End date.
+};
+
+/******************************************************************************
 **
 ** Implementation of inline functions.
 **
 *******************************************************************************
 */
+
+inline CIsSessionInRange::CIsSessionInRange(const CDateTime& dtStart, const CDateTime& dtEnd)
+	: m_dtStart(dtStart)
+	, m_dtEnd(dtEnd)
+{
+}
+
+inline bool CIsSessionInRange::operator()(const CSession* pSession)
+{
+	ASSERT(pSession != NULL);
+
+	return ( (pSession->Start() >= m_dtStart) && (pSession->Start() < m_dtEnd) );
+}
 
 #endif //SESSLIST_HPP
