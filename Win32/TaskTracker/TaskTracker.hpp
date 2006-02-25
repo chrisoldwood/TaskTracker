ca84c31063d39333cf9a4cb22569c1c806c92fe5
Administrator <administrator@chrisoldwood.com>
2006-02-25 21:03:52 +0000

    Removed member accessor methods and made the data members public as the accesors effectively did this. Replaced the #defines for the filenames and file version with local constants as they are used within the .cpp file. Added new configurable member to enable warnings on sessions that overlap.

--- Win32/TaskTracker/TaskTracker.hpp
+++ Win32/TaskTracker/TaskTracker.hpp
@@ -51,11 +51,11 @@ typedef enum tagPeriod
 *******************************************************************************
 */
 
-typedef enum tagMinsFormat
+typedef enum tagLenFormat
 {
 	HoursMins,
 	HoursOnly,
-} MinsFormat;
+} LenFormat;
 
 /******************************************************************************
 ** 
@@ -92,61 +92,29 @@ public:
 	~CTaskTracker();
 
 	//
-	// Member access
-	//
-	CSessionList& SessionList();
-	CTaskList& TaskList();
-	CLocnList& LocnList();
-
-	bool ClockedIn() const;
-
-	const CSessionPtr& CurrentSession() const;
-
-	const CString& LastTask() const;
-	void SetLastTask(const CString& strTask);
-
-	const CString& LastLocn() const;
-	void SetLastLocn(const CString& strLocn);
-
-	bool IsModified() const;
-	void Modified();
-
-	//
-	// Preferences access.
-	//
-	CPrinter& Printer();
-	const char* DefaultFile() const;
-	Grouping DefaultGrouping() const;
-	Period DefaultPeriod() const;
-	bool MinimiseToTray() const;
-
-	//
-	// Preferences setting.
-	//
-	void DefaultFile(const char* pszPath);
-	void DefaultGrouping(Grouping eGrouping);
-	void DefaultPeriod(Period ePeriod);
-	bool MinimiseToTray(bool bMinToTray);
-
-	//
-	// Session status.
+	// Session state.
 	//
 	void ClockIn (const CDateTime& dtIn,  const CString& strTask, const CString& strLocn);
 	void ClockOut(const CDateTime& dtOut, const CString& strTask, const CString& strLocn);
+
+	void DeleteAllData();
+
+	//
+	// Session reporting.
+	//
 	ulong TotalForPeriod(const CDateTime& dtStart, const CDateTime& dtEnd) const;
 	ulong TotalForDay(const CDate& rDate) const;
 	ulong TotalForWeek(const CDate& rDate) const;
 	ulong TotalForMonth(const CDate& rDate) const;
 	ulong TotalOverall() const;
 
-	void DeleteAllData();
+	void ReportData(CReport& rDevice, Grouping eGrouping, const CDate& rFromDate, const CDate& rToDate) const;
 
 	//
 	// Serialisation.
 	//
 	bool LoadData();
 	bool SaveData();
-	void ReportData(CReport& rDevice, Grouping eGrouping, const CDate& rFromDate, const CDate& rToDate) const;
 
 	//
 	// Misc methods.
@@ -160,10 +128,38 @@ public:
 	static const char* DateTimePickerFormat();
 
 	//
-	// Members
+	// UI members
 	//
-	CAppWnd		m_AppWnd;
-	CAppCmds	m_AppCmds;
+	CAppWnd			m_AppWnd;
+	CAppCmds		m_AppCmds;
+	CPrinter		m_oPrinter;
+
+	//
+	// Data members
+	//
+	CSessionList	m_oSessionList;
+	CTaskList		m_oTaskList;
+	CLocnList		m_oLocnList;
+	int16			m_bClockedIn;
+	CSessionPtr		m_pCurrSession;
+	CString			m_strLastTask;
+	CString			m_strLastLocn;
+	bool			m_bModified;
+
+	//
+	// Preferences members.
+	//
+	CIniFile		m_IniFile;
+	Grouping		m_eDefGrouping;
+	Period			m_eDefPeriod;
+	LenFormat		m_eLenFormat;
+	bool			m_bMinToTray;
+	CString			m_strReportFile;
+	CString			m_strExportFile;
+	CString			m_strImportFile;
+	CRect			m_rcReportDlg;
+	CRect			m_rcEditDlg;
+	bool			m_bCheckOverlap;
 
 	//
 	// Constants.
@@ -176,29 +172,6 @@ protected:
 	//
 	virtual	bool OnOpen();
 	virtual	bool OnClose();
-
-	//
-	// Members
-	//
-	CSessionList	m_SessionList;
-	CTaskList		m_TaskList;
-	CLocnList		m_LocnList;
-	int16			m_bClockedIn;
-	CSessionPtr		m_pCurrSession;
-	CString			m_strLastTask;
-	CString			m_strLastLocn;
-	bool			m_bModified;
-	CPrinter		m_Printer;
-
-	//
-	// Preferences.
-	//
-	CIniFile		m_IniFile;
-	CString			m_strDefFile;
-	Grouping		m_eDefGrouping;
-	Period			m_eDefPeriod;
-	MinsFormat		m_eMinsFormat;
-	bool			m_bMinToTray;
 
 	//
 	// Internal methods.
@@ -217,21 +190,6 @@ protected:
 	void SaveDefaults();
 };
 
-// The title for the .dat and .ini files.
-#define FILE_TITLE			"TaskTrak"
-
-// The current file version.
-#define DAT_FILE_VERSION	35
-
-// The data filename.
-#define	DAT_FILE_NAME		FILE_TITLE ".dat"
-
-// The cfg filename.
-#define INI_FILE_NAME		FILE_TITLE ".ini"
-
-// The default report filename.
-#define	RPT_FILE_NAME		FILE_TITLE ".txt"
-
 /******************************************************************************
 **
 ** Global variables.
@@ -241,112 +199,5 @@ protected:
 
 //The application object.
 extern CTaskTracker App;
-
-/******************************************************************************
-**
-** Implementation of inline functions.
-**
-*******************************************************************************
-*/
-
-inline bool CTaskTracker::ClockedIn() const
-{
-	return m_bClockedIn;
-}
-
-inline CTaskList& CTaskTracker::TaskList()
-{
-	return m_TaskList;
-}
-
-inline const CString& CTaskTracker::LastTask() const
-{
-	return m_strLastTask;
-}
-
-inline void CTaskTracker::SetLastTask(const CString& strTask)
-{
-	m_strLastTask = strTask;
-}
-
-inline CSessionList& CTaskTracker::SessionList()
-{
-	return m_SessionList;
-}
-
-inline const CSessionPtr& CTaskTracker::CurrentSession() const
-{
-	return m_pCurrSession;
-}
-
-inline CLocnList& CTaskTracker::LocnList()
-{
-	return m_LocnList;
-}
-
-inline const CString& CTaskTracker::LastLocn() const
-{
-	return m_strLastLocn;
-}
-
-inline void CTaskTracker::SetLastLocn(const CString& strLocn)
-{
-	m_strLastLocn = strLocn;
-}
-
-inline bool CTaskTracker::IsModified() const
-{
-	return m_bModified;
-}
-
-inline void CTaskTracker::Modified()
-{
-	m_bModified = true;
-}
-
-inline CPrinter& CTaskTracker::Printer()
-{
-	return m_Printer;
-}
-
-inline const char* CTaskTracker::DefaultFile() const
-{
-	return m_strDefFile;
-}
-
-inline Grouping CTaskTracker::DefaultGrouping() const
-{
-	return m_eDefGrouping;
-}
-
-inline Period CTaskTracker::DefaultPeriod() const
-{
-	return m_eDefPeriod;
-}
-
-inline bool CTaskTracker::MinimiseToTray() const
-{
-	return m_bMinToTray;
-}
-
-inline void CTaskTracker::DefaultFile(const char* pszPath)
-{
-	m_strDefFile = pszPath;
-}
-
-inline void CTaskTracker::DefaultGrouping(Grouping eGrouping)
-{
-	m_eDefGrouping = eGrouping;
-}
-
-inline void CTaskTracker::DefaultPeriod(Period ePeriod)
-{
-	m_eDefPeriod = ePeriod;
-}
-
-inline bool CTaskTracker::MinimiseToTray(bool bMinToTray)
-{
-	m_bMinToTray = bMinToTray;
-}
 
 #endif //TASKTRACKER_HPP
